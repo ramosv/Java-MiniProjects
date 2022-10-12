65e16200883449361191c28840dd0f11db63ec32
ramosv <56368263+ramosv@users.noreply.github.com>
2022-10-11 22:12:23 -0600

    Country and capital with private attributes

---
+++ CountryCapital.java
@@ -0,0 +1,31 @@
+import java.util.concurrent.ThreadPoolExecutor.CallerRunsPolicy;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+    }
+}
+
+class Country {
+    private String name;
+    private String capital;
+
+    public String getName() {
+
+        return name;
+    }
+
+    public String setName(string i) {
+        name = i;
+    }
+
+    public String getCapital() {
+        return capital;
+    }
+
+    public String setCapital(string i) {
+        capital = i;
+    }
+
+}
