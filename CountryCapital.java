b2f883b9a3ea4910674aa8e52a65d16de0fa2969
ramosv <56368263+ramosv@users.noreply.github.com>
2022-10-11 22:17:03 -0600

    Getter and Setters

--- CountryCapital.java
+++ CountryCapital.java
@@ -4,6 +4,20 @@ public class Program {
 
     public static void main(String[] args) {
 
+        Country chile = new Country();
+        Country france = new Country();
+
+        france.setName("France");
+        france.setCapital("Paris");
+        chile.setName("Chile");
+        chile.setCapital("Santiago");
+
+        System.out.println(chile.getName());
+        System.out.println(chile.getCapital());
+
+        System.out.println(france.getName());
+        System.out.println(france.getCapital());
+
     }
 }
 
