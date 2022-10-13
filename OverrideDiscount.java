e3c2b52429666dfe432c54e59173674af73b7bee
ramosv <56368263+ramosv@users.noreply.github.com>
2022-10-12 23:47:31 -0600

    OverrideDiscount

---
+++ OverrideDiscount.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
+
+class customerSale {
+    // int cost;
+
+    public int discount(int cost) {
+        cost = x - (cost * 10) / 100;
+        return cost;
+    }
+
+}
