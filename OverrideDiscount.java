676b67a29514f73d8294aacf523a5db2f8f51adc
ramosv <56368263+ramosv@users.noreply.github.com>
2022-10-12 23:55:20 -0600

    Use @Override to override the methord

--- OverrideDiscount.java
+++ OverrideDiscount.java
@@ -2,6 +2,21 @@ public class Main {
 
     public static void main(String[] args) {
 
+        customerSale newSale = new customerSale();
+        customerSale secondSale = new customerSale() {
+            @Override
+            public int discount(int c) {
+                c = c - (c * 20) / 100;
+                return c;
+
+            }
+        };
+        // give the second sale of the day a discount by using @Override
+
+        int totalSale1 = newSale.discount(9999);
+        int totalSale2 = secondSale.discount(9999);
+        System.out.println(totalSale1, totalSale2);
+
     }
 }
 
@@ -9,7 +24,7 @@ class customerSale {
     // int cost;
 
     public int discount(int cost) {
-        cost = x - (cost * 10) / 100;
+        cost = cost - (cost * 10) / 100;
         return cost;
     }
 
