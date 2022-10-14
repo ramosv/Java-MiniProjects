6d05be0971d78a5b31b42ed262ef3bf5e3d44446
ramosv <56368263+ramosv@users.noreply.github.com>
2022-10-13 23:35:11 -0600

    Objects and classes

--- CarPolymorphism.java
+++ CarPolymorphism.java
@@ -1,10 +1,42 @@
 class Main {
 
     public static void main(String[] args) {
+        car normal = new car();
+        normal.run();
+        normal.fuel();
+
+        car electric = new electricCar();
+        car hybrid = new hybridCar();
+
+        electric.run();
+        electric.fuel();
+        hybrid.run();
+        hybrid.fuel();
 
     }
 }
 
 class car {
 
+    public void run() {
+        System.out.println("Starting");
+    }
+
+    public void fuel() {
+        System.out.println("This car runs on fossil fuels");
+    }
+
+}
+
+class electricCar extends car {
+    public void fuel() {
+        System.out.println("This car runs on electricity");
+    }
+
+}
+
+class hybridCar extends car {
+    public void fuel() {
+        System.out.println("This car runs on both gas and electricity");
+    }
 }
