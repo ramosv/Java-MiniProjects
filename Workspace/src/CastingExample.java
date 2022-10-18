bb8eea32f370723ac56720ae647b8ee76e4c795a
ramosv <56368263+ramosv@users.noreply.github.com>
2022-10-18 00:21:03 -0600

    Adjusted structure

---
+++ Workspace/src/CastingExample.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class CastingExample {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        char i = input.next().charAt(0);
+
+        int j = (int) i;
+
+        System.out.println(j);
+    }
+
+}
