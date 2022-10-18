277a9e6472780a5e56a895650d7a01be1965f436
ramosv <56368263+ramosv@users.noreply.github.com>
2022-10-17 23:55:01 -0600

    Fixed errors

---
+++ CastingExample.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class Main {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        char i = input.next().charAt(0);
+
+        int j = (int) i;
+
+        System.out.println(j);
+    }
+}
