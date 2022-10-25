0f037293cd266bc355ec0e2ba61dbad8a1f2c6dc
ramosv <56368263+ramosv@users.noreply.github.com>
2022-10-24 22:53:36 -0600

    Factorial number

--- Workspace/src/EasyRecursion.java
+++ Workspace/src/EasyRecursion.java
@@ -1,16 +1,24 @@
+
+import java.util.*;
+
 public class EasyRecursion {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int input = sc.nextInt();
+
+        System.out.println(fact.factorial(input));
 
     }
 
 }
 
-public int factorial(int x){
-    if(x ==1){
-        return 1;
-    }
-    else{
-    return x * factorial(- 1);
+class fact {
+    public static int factorial(int x) {
+        if (x == 1) {
+            return 1;
+        } else {
+            return x * factorial(x - 1);
+        }
     }
 }
