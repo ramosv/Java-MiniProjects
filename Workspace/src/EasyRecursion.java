ccad2551663dec64d945f215e96bb89bf42ebcc4
ramosv <56368263+ramosv@users.noreply.github.com>
2022-10-24 22:42:17 -0600

    Recursion example w/ error

---
+++ Workspace/src/EasyRecursion.java
@@ -0,0 +1,16 @@
+public class EasyRecursion {
+
+    public static void main(String[] args) {
+
+    }
+
+}
+
+public int factorial(int x){
+    if(x ==1){
+        return 1;
+    }
+    else{
+    return x * factorial(- 1);
+    }
+}
