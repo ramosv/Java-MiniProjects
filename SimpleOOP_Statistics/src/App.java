c4d7095e3b0a0d9cfb760bfbfdfc558d5561f54f
ramosv <56368263+ramosv@users.noreply.github.com>
2022-11-28 13:43:56 -0700

    Main Class

--- SimpleOOP_Statistics/src/App.java
+++ SimpleOOP_Statistics/src/App.java
@@ -1,5 +1,35 @@
+import java.util.Scanner;
+
 public class App {
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // System.out.println("Hello, World!");
+
+        Scanner sc = new Scanner(System.in);
+
+        Statistics statistics = new Statistics();
+        Statistics even = new Statistics();
+        Statistics odd = new Statistics();
+
+        System.out.println("Enter numbers: ");
+
+        while (true) {
+            int input = Integer.valueOf(sc.nextLine());
+            if (input == -1) {
+                break;
+            } else if (input % 2 == 0) {
+                statistics.addNumber(input);
+                even.addNumber(input);
+            } else if (input % 2 != 0) {
+                statistics.addNumber(input);
+                odd.addNumber(input);
+            }
+
+        }
+
+        System.out.println("Sum: " + statistics.sum());
+        System.out.println("Sum of even numbers: " + even.sum());
+        System.out.println("Sum of odd numbers: " + odd.sum());
+
     }
 }
