39ca3381c97417d75c7b3b4bf8703bbf867317a6
ramosv <56368263+ramosv@users.noreply.github.com>
2022-11-29 17:30:14 -0700

    Main class

--- SimpleOOP_BookLibrary/src/App.java
+++ SimpleOOP_BookLibrary/src/App.java
@@ -1,5 +1,36 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        ArrayList<Book> books = new ArrayList<Book>();
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Title: ");
+            String name = sc.nextLine();
+            if (name.isEmpty()) {
+                break;
+            }
+            System.out.println("Pages: ");
+            int page = Integer.valueOf(sc.nextLine());
+            System.out.println("Publication Year: ");
+            int year = Integer.valueOf(sc.nextLine());
+
+            books.add(new Book(name, page, year));
+        }
+
+        System.out.println("What information will be printed? ");
+        String input = sc.nextLine();
+
+        for (Book i : books) {
+            if (input.equals("everything")) {
+                System.out.println(i.toString());
+
+            } else if (input.equals("name")) {
+                System.out.println(i.getTitle());
+            }
+
+        }
     }
 }
