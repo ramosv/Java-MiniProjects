98fcbc988bcaa881211ed89af1fde7764be46bd4
ramosv <56368263+ramosv@users.noreply.github.com>
2022-11-29 17:12:25 -0700

    Main program + testing

--- SimpleOOP_TelevisionProgram/src/App.java
+++ SimpleOOP_TelevisionProgram/src/App.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        ArrayList<TelevisionProgram> programs = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Name: ");
+            String name = scanner.nextLine();
+            if (name.isEmpty()) {
+                break;
+            }
+            System.out.println("Duration: ");
+            int time = Integer.valueOf(scanner.nextLine());
+
+            programs.add(new TelevisionProgram(name, time));
+        }
+
+        System.out.println("Program's maximum duration? ");
+        int duration = Integer.valueOf(scanner.nextLine());
+
+        for (TelevisionProgram i : programs) {
+            if (i.getDuration() <= duration) {
+                System.out.println(i.toString());
+            }
+        }
     }
 }
