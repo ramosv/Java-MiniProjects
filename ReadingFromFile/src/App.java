137f48f65a64be48683e29513774961ef2fd1b4e
ramosv <56368263+ramosv@users.noreply.github.com>
2022-12-01 14:12:39 -0700

    Reading from data.txt and printing results

--- ReadingFromFile/src/App.java
+++ ReadingFromFile/src/App.java
@@ -3,6 +3,20 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Name of the file: ");
+        String file = scanner.nextLine();
+
+        try (Scanner sc = new Scanner(Paths.get(file))) {
+            while (sc.hasNextLine()) {
+                String row = sc.nextLine();
+
+                String[] pieces = row.split(",");
+
+                System.out.println(pieces[0] + " age:" + pieces[1] + " years");
+            }
+        } catch (Exception e) {
+            System.out.println("Error: " + e);
+        }
     }
 }
