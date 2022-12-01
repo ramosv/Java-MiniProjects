23562e12a87adb4732af1084e9dfc0ef8b4012e1
ramosv <56368263+ramosv@users.noreply.github.com>
2022-12-01 15:40:39 -0700

    Main class + test

--- OOP-ReadingFromFile/src/App.java
+++ OOP-ReadingFromFile/src/App.java
@@ -1,5 +1,42 @@
+
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Filename:");
+        String file = scan.nextLine();
+
+        ArrayList<Person> records = readRecordsFromFile(file);
+        System.out.println("Persons: " + records.size());
+        System.out.println("Persons:");
+        for (Person person : records) {
+            System.out.println(person);
+
+        }
+    }
+
+    public static ArrayList<Person> readRecordsFromFile(String file) {
+        ArrayList<Person> persons = new ArrayList<>();
+
+        try (Scanner sc = new Scanner(Paths.get(file))) {
+            while (sc.hasNextLine()) {
+                String row = sc.nextLine();
+                String[] arr = row.split(",");
+                int age = Integer.valueOf(arr[1]);
+
+                persons.add(new Person(arr[0], age));
+
+            }
+        } catch (Exception e) {
+            System.out.println("Error: " + e);
+
+        }
+
+        return persons;
+
     }
 }
