40bd722f1e40eea9a6fac89358b0824d4f24acb5
ramosv <56368263+ramosv@users.noreply.github.com>
2022-12-01 14:10:52 -0700

    Reading from a file with comma separated values

---
+++ ReadingFromFile/src/App.java
@@ -0,0 +1,8 @@
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
