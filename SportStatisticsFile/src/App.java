1d6daf1576745bcd829b9be20693eacac80035bb
ramosv <56368263+ramosv@users.noreply.github.com>
2022-12-02 09:34:57 -0700

    Sports Stats main program

--- SportStatisticsFile/src/App.java
+++ SportStatisticsFile/src/App.java
@@ -1,5 +1,49 @@
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("File: ");
+        String file = scan.nextLine();
+        System.out.println("Team: ");
+        String team = scan.nextLine();
+        int games = 0;
+        int win = 0;
+        int losses = 0;
+
+        try (Scanner sc = new Scanner(Paths.get(file))) {
+
+            while (sc.hasNextLine()) {
+                String row = sc.nextLine();
+                String[] pieces = row.split(",");
+                int h = Integer.valueOf(pieces[2]);
+                int a = Integer.valueOf(pieces[3]);
+
+                if (pieces[0].equals(team)) {
+                    games++;
+                    if (h > a) {
+                        win++;
+                    } else {
+                        losses++;
+                    }
+                } else if (pieces[1].equals(team)) {
+                    games++;
+                    if (h > a) {
+                        losses++;
+                    } else {
+                        win++;
+                    }
+
+                }
+            }
+
+        } catch (Exception e) {
+            System.out.println("Error: " + e);
+        }
+        System.out.println("Games: " + games);
+        System.out.println("Wins: " + win);
+        System.out.println("Losses: " + losses);
     }
 }
