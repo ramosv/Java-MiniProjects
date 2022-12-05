cfc046399ed340d4e7df8eee26341f3d22df5cdd
ramosv <56368263+ramosv@users.noreply.github.com>
2022-12-04 23:02:11 -0700

    Clock Class constructor

--- Clock_Timer/src/Clock.java
+++ Clock_Timer/src/Clock.java
@@ -1,7 +1,27 @@
+import java.time.Clock;
+import java.util.Locale;
+
 public class Clock {
 
     private ClockHand hour;
     private ClockHand minute;
     private ClockHand second;
+
+    public Clock() {
+        this.hour = new ClockHand(24);
+        this.minute = new ClockHand(60);
+        this.second = new ClockHand(60);
+    }
+
+    public void advance() {
+        this.second.advance();
+        if (this.second.getValue() == 0) {
+            this.minute.advance();
+            if (this.minute.getValue() == 0) {
+                this.hour.advance();
+            }
+        }
+
+    }
 
 }
