16e6da67f79a40a9d2b2f7423282a46b19b65551
ramosv <56368263+ramosv@users.noreply.github.com>
2022-12-06 08:49:03 -0700

    Main app class

--- Fitbyte/src/App.java
+++ Fitbyte/src/App.java
@@ -1,5 +1,14 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Fitbyte fitbyte = new Fitbyte(30, 60);
+
+        double percentage = 0.5;
+
+        while (percentage < 1.0) {
+            double target = fitbyte.targetHeartRate(percentage);
+            System.out.println("Target " + (percentage * 100) + "% of maximum: " + target);
+            percentage += 0.1;
+        }
+
     }
 }
