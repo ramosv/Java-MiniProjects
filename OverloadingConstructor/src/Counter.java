489e81b28185b1cea906af6848fa8dd341eaa608
ramosv <56368263+ramosv@users.noreply.github.com>
2022-12-07 14:41:05 -0700

    Counter class

---
+++ OverloadingConstructor/src/Counter.java
@@ -0,0 +1,39 @@
+public class Counter {
+
+    private int value;
+
+    public Counter(int startValue) {
+        this.value = startValue;
+    }
+
+    public Counter() {
+        this.value = 0;
+    }
+
+    public int value() {
+        return this.value;
+    }
+
+    public void increase() {
+        this.value += 1;
+    }
+
+    public void increase(int increaseBy) {
+        if (increaseBy < 0) {
+        } else {
+            this.value += increaseBy;
+        }
+    }
+
+    public void decrease() {
+        this.value -= 1;
+    }
+
+    public void decrease(int decreaseBy) {
+        if (decreaseBy < 0) {
+        } else {
+            this.value -= decreaseBy;
+        }
+    }
+
+}
