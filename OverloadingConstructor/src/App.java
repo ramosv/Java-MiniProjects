0b61e14a1dbb69871fcb06048ffd36a131a855f7
ramosv <56368263+ramosv@users.noreply.github.com>
2022-12-09 09:30:00 -0700

    Payment Card

--- OverloadingConstructor/src/App.java
+++ OverloadingConstructor/src/App.java
@@ -1,5 +1,18 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Counter testing = new Counter();
+        Counter count = new Counter(100);
+
+        testing.increase(50);
+        testing.increase();
+        testing.decrease(15);
+
+        System.out.println(testing.value());
+
+        count.decrease(14);
+        count.decrease();
+        count.increase(5);
+
+        System.out.println(count.value());
     }
 }
