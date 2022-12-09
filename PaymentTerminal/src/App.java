b2a10b076ff661d2da65e294a72d54bc86dfaed2
ramosv <56368263+ramosv@users.noreply.github.com>
2022-12-09 09:45:35 -0700

    Main class testing

--- PaymentTerminal/src/App.java
+++ PaymentTerminal/src/App.java
@@ -1,5 +1,22 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        PaymentTerminal coffeeShop = new PaymentTerminal();
+        System.out.println(coffeeShop);
+
+        PaymentCard viceCard = new PaymentCard(2);
+
+        System.out.println("amount of money on the card is " + viceCard.balance() + " dollars");
+
+        boolean wasSuccessful = coffeeShop.eatHeartily(viceCard);
+        System.out.println("there was enough money: " + wasSuccessful);
+
+        coffeeShop.addMoneyToCard(viceCard, 100);
+
+        wasSuccessful = coffeeShop.eatHeartily(viceCard);
+        System.out.println("there was enough money: " + wasSuccessful);
+
+        System.out.println("amount of money on the card is " + viceCard.balance() + " dollars");
+
+        System.out.println(coffeeShop);
     }
 }
