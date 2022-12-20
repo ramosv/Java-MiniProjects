3c196b1dd82a8042ded27dc95ecc186555a7cd74
ramosv <56368263+ramosv@users.noreply.github.com>
2022-12-20 08:53:28 -0700

    Test in Main class

--- SimpleDictionary/src/App.java
+++ SimpleDictionary/src/App.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+        SimpleDictionary dictionary = new SimpleDictionary();
+
+        TextUI textUI = new TextUI(scanner, dictionary);
+        textUI.start();
+        System.out.println(dictionary.translate("pike"));
     }
 }
