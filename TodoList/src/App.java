d48ea70329cdbe7d35e1d7bb21ee1beaf3ce86a6
ramosv <56368263+ramosv@users.noreply.github.com>
2022-12-21 19:10:21 -0700

    Testing

--- TodoList/src/App.java
+++ TodoList/src/App.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        TodoList list = new TodoList();
+        Scanner scanner = new Scanner(System.in);
+
+        UserInterface ui = new UserInterface(list, scanner);
+        ui.start();
     }
 }
