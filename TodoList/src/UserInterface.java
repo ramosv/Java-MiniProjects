8bae40b46cc5bdafdb53e35cd0bbb25afb13f62f
ramosv <56368263+ramosv@users.noreply.github.com>
2022-12-21 19:11:41 -0700

    Removed redundant code.

--- TodoList/src/UserInterface.java
+++ TodoList/src/UserInterface.java
@@ -17,32 +17,38 @@ public class UserInterface {
             if (input.equals("stop"))
                 break;
             else if (input.equals("add")) {
-                add();
+                System.out.println("To add: ");
+                String add = sc.nextLine();
+                this.list.add(add);
             } else if (input.equals("list")) {
-                list();
+                this.list.print();
             } else if (input.equals("remove")) {
-                remove();
+                System.out.println("Which one is removed? ");
+                int key = Integer.valueOf(sc.nextLine());
+                this.list.remove(key);
             } else {
                 continue;
             }
         }
 
     }
-
-    public void add() {
-        System.out.println("To add: ");
-        String add = sc.nextLine();
-        this.list.add(add);
-    }
-
-    public void list() {
-        this.list.print();
-    }
-
-    public void remove() {
-        System.out.println("Which one is removed? ");
-        int key = Integer.valueOf(sc.nextLine());
-        this.list.remove(key);
-    }
+    // redundant code, it works tho.
+    /*
+     * public void add() {
+     * System.out.println("To add: ");
+     * String add = sc.nextLine();
+     * this.list.add(add);
+     * }
+     * 
+     * public void list() {
+     * this.list.print();
+     * }
+     * 
+     * public void remove() {
+     * System.out.println("Which one is removed? ");
+     * int key = Integer.valueOf(sc.nextLine());
+     * this.list.remove(key);
+     * }
+     */
 
 }
