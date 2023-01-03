fc898798fbe3882c0f3de7a5acd492b35dc05154
ramosv <56368263+ramosv@users.noreply.github.com>
2023-01-02 21:58:46 -0700

    Selection Sort - OOP approach

--- ExerciseManager/src/MainProgram.java
+++ ExerciseManager/src/MainProgram.java
@@ -1,7 +1,6 @@
 
 public class MainProgram {
 
-    // update here your exercise progress
     public static int partsCompleted() {
         return 0;
     }
---
+++ SelectionSort/src/App.java
@@ -0,0 +1,43 @@
+import java.util.Arrays;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        int[] arr = { 5, 56, 2, 7, 354, 85, 35, 681, 89, 6 };
+        sort(arr);
+
+    }
+    // Divide into three different pieces/methods
+    // Index of the Smallest starting at a specified index
+    // Swap operation
+    // Sort operation
+
+    public static int indexOfSmallest(int[] arr, int indexFrom) {
+        int index = 0;
+        int temp = 999999;
+
+        for (int i = indexFrom; i < arr.length; i++) {
+            if (arr[i] < temp) {
+                temp = arr[i];
+                index = i;
+            }
+        }
+        return index;
+
+    }
+
+    public static void swap(int[] arr, int index1, int index2) {
+        int temp = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = temp;
+    }
+
+    public static void sort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            swap(arr, i, indexOfSmallest(arr, i));
+            // print to see progress after each swap
+            System.out.println(Arrays.toString(arr));
+        }
+    }
+
+}
