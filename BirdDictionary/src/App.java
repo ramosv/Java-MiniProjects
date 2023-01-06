69a894a592e7868230567b38116d8f70c9b2b299
ramosv <56368263+ramosv@users.noreply.github.com>
2023-01-05 23:33:46 -0700

    Main class - UI

--- BirdDictionary/src/App.java
+++ BirdDictionary/src/App.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        Bird bird = new Bird(scan);
+
+        while (true) {
+            System.out.println("?");
+            String input = scan.nextLine();
+            if (input.equals("Quit"))
+                break;
+            else if (input.equals("Add")) {
+                bird.add();
+            } else if (input.equals("Observation")) {
+                bird.observation();
+            } else if (input.equals("All")) {
+                bird.all();
+            } else if (input.equals("One")) {
+                bird.one();
+            }
+
+        }
+
     }
 }
