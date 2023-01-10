fe7463e6e3b2516a9143cab606cdf435b469eb47
ramosv <56368263+ramosv@users.noreply.github.com>
2023-01-09 23:55:32 -0700

    Testing program

--- CookingBook/src/App.java
+++ CookingBook/src/App.java
@@ -1,5 +1,57 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.nio.file.Paths;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("File to read:");
+        String file = scanner.nextLine();
+
+        Recipe list = new Recipe();
+
+        try (Scanner sc = new Scanner(Paths.get(file))) {
+            while (sc.hasNextLine()) {
+                String row = sc.nextLine();
+                list.readFile(row);
+            }
+            list.list();
+        } catch (Exception e) {
+            System.out.println("Error: " + e);
+        }
+
+        System.out.println("Commands: ");
+        System.out.println("list - lists of recipes");
+        System.out.println("stop - stops the program");
+        System.out.println("find name - searches recipes by name");
+        System.out.println("find cooking time - searches recipes by cooking time");
+        System.out.println("find ingredient - searches recipes by ingredient");
+        System.out.println("");
+
+        while (true) {
+            String input = scanner.nextLine();
+            System.out.println("Enter command: ");
+            if (input.equals("stop"))
+                break;
+            else if (input.equals("list")) {
+                list.listPrint();
+            } else if (input.equals("find name")) {
+                System.out.println("Searched word: ");
+                String next = scanner.nextLine();
+                list.findName(next);
+            } else if (input.equals("find cooking time")) {
+                System.out.println("Max cooking time: ");
+                String next = scanner.nextLine();
+                list.findTime(next);
+            } else if (input.equals("find ingredient")) {
+                System.out.println("Ingredient: ");
+                String next = scanner.nextLine();
+                list.findIngredient(next);
+            }
+        }
+
     }
+
 }
--- CookingBook/src/SingleRecipe.java
+++ CookingBook/src/SingleRecipe.java
@@ -1,3 +1,65 @@
+import java.util.ArrayList;
+
 public class SingleRecipe {
+
+    private String name;
+    private String time;
+    private String ingredients;
+
+    public SingleRecipe(String name, String time, String ingre) {
+        this.name = name;
+        this.time = time;
+        this.ingredients = ingre;
+    }
+
+    public void findName(String name) {
+
+        String[] pieces = this.name.split("");
+        if (this.name.contains(name)) {
+            System.out.println(this.name + ", cooking time: " + this.time);
+        } else {
+            for (int x = 0; x < pieces.length; x++) {
+                if (pieces[x].equals(name)) {
+                    System.out.println(this.name + ", cooking time: " + this.time);
+
+                }
+            }
+        }
+    }
+
+    public void findTime() {
+        System.out.println(this.name + ", cooking time: " + this.time);
+
+    }
+
+    public String getIngredient() {
+        return this.ingredients;
+    }
+
+    public void findIngredients(String ingre) {
+        ArrayList<String> list = new ArrayList<>();
+        String[] pieces = this.ingredients.split(" ");
+        for (int x = 0; x < pieces.length; x++) {
+            list.add(pieces[x]);
+        }
+        for (String x : list) {
+            if (x.equals(ingre)) {
+                System.out.println(this.name + ", cooking time: " + this.time);
+            }
+        }
+    }
+
+    public void printList() {
+
+    }
+
+    public int getTime() {
+        int t = Integer.valueOf(this.time);
+        return t;
+    }
+
+    public String toString() {
+        return this.name + ", cooking time: " + this.time;
+    }
 
 }
